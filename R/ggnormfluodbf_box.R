4f7e72fceb32ced4e891e5ed89e576f1992db786
alphaprime7 <awesome.tingwei@outlook.com>
2024-09-03 13:24:28 -0500

    Initial commit

--- R/ggnormfluodbf_box.R
+++ R/ggnormfluodbf_box.R
@@ -1,17 +1,70 @@
 #' Ggnormfluodbf Box Plot
 #' @param data data
 #' @param mapping mapping
+#' @param alpha alpha
+#' @param include_mean mean boolean
+#' @param dodge_pos dodge_pos
+#' @param vadj vertical adjustment
+#' @param hadj horizontal adjustment
 #' @return ggplot object
 #' @export
-#'
 #' @examples
+#' \dontrun{
+#' ggnormfluodbf_box(tips,ggplot2::aes(x = smoker, y = tip, fill = sex))
+#' ggnormfluodbf_box(tips,ggplot2::aes(x=smoker, y = tip, fill = sex), alpha = 2)
+#' ggnormfluodbf_box(tips,ggplot2::aes(x=smoker, y = tip, fill = sex), alpha = 2, vadj = -5, hadj = 0.7)
+#' }
 ggnormfluodbf_box <- function(data,
-                              mapping){
+                              mapping,
+                              alpha = 0.5,
+                              include_mean = TRUE,
+                              include_labels = TRUE,
+                              dodge_pos = 0.75,
+                              vadj = -0.5,
+                              hadj = 0.5){
   mapping$x <- mapping$x
   mapping$y <- mapping$y
   mapping$fill <- mapping$fill
 
-  p <- ggplot2::ggplot(data, mapping) +
-    ggplot2::geom_boxplot()
+  if (is.null(include_mean) || include_mean){
+    p <- ggplot2::ggplot(data, mapping) +
+      ggplot2::geom_boxplot(alpha = alpha) +
+      ggplot2::stat_summary(fun=mean,
+                            mapping = ggplot2::aes(color=mapping$fill),
+                            geom="point",
+                            position = ggplot2::position_dodge2(width = 0.75, preserve = "single"),
+                            shape=20,
+                            size=3,
+                            color="red"
+      )
+  } else {
+    p <- ggplot2::ggplot(data, mapping) +
+      ggplot2::geom_boxplot(alpha = alpha)
+  }
+
+  if (is.null(include_labels) || include_labels) {
+    p <- p +
+      ggplot2::theme(legend.position='none') +
+      ggplot2::geom_text(
+        stat = "summary",
+        fun = median,
+        mapping = ggplot2::aes(label = sprintf("median = %.2f", ggplot2::after_stat(y) )),
+        position = ggplot2::position_dodge(width = dodge_pos),
+        vjust = vadj,
+        hjust = hadj
+      ) +
+      ggplot2::geom_text(
+        stat = "summary",
+        fun = mean,
+        mapping = ggplot2::aes(label = sprintf("mean = %.2f", ggplot2::after_stat(y) )),
+        position = ggplot2::position_dodge(width = dodge_pos),
+        vjust = vadj + 2.5,
+        hjust = hadj
+      )
+    p
+  }
+  else {
+    p
+  }
   p
 }
